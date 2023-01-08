ce11fcccc3b7865ccae2ecf4be05944ff9c0111b
Yizhou Zhang <essiazhang@gmail.com>
2023-01-08 14:37:52 -0800

    fixed mobile view and remove redux when logout.

--- react-app/src/components/Auth/LogoutButton.js
+++ react-app/src/components/Auth/LogoutButton.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { removeAllFriends } from '../../store/friend';
+import { removeAllOpenRquests, removeOpenRequest } from '../../store/openRequest';
 import { logout } from '../../store/session';
+import { removeFriendTransactions, removeTransaction, removeUserTransactions } from '../../store/transactons';
+import { removeAllUsers, removeNonFriendUsers, removeSelectedUser } from '../../store/user';
 import './Auth.css'
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
-  const onLogout = async (e) => {
-    await dispatch(logout());
+  const onLogout = (e) => {
+    dispatch(logout())
+      .then(() => {
+        dispatch(removeSelectedUser());
+        dispatch(removeAllUsers());
+        dispatch(removeNonFriendUsers());
+        dispatch(removeUserTransactions());
+        dispatch(removeFriendTransactions());
+        dispatch(removeTransaction());
+        dispatch(removeOpenRequest());
+        dispatch(removeAllOpenRquests());
+        dispatch(removeAllFriends());
+      });
   };
 
   return <button onClick={onLogout} className='logout-butt'>Logout</button>;
