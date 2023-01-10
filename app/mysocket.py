76cf6ba3bcd014d6a285dac0c7716c8692ba0acb
Yizhou Zhang <essiazhang@gmail.com>
2023-01-09 22:35:53 -0800

    eventlet version and monkey-patch

--- app/mysocket.py
+++ app/mysocket.py
@@ -1,5 +1,8 @@
 from flask_socketio import SocketIO, emit, join_room
 import os
+import eventlet
+
+eventlet.monkey_patch(socket= True)
 
 # cors setup to allow my website only under production, but everything under development
 if os.environ.get("FLASK_ENV") == "production":
